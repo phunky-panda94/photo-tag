1a3139f2c5b3b7b3d86c8fc5cb520bf3cd8859d3
phunky-panda94 <victor.nim94@outlook.com>
2021-11-08 14:08:57 +1100

    implemented outline on click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="container flex flex-col flex-ai-c">
-        <h1>Guess Who?</h1>
-        <div className="photo flex flex-col flex-jc-c">
-            <img alt="group" src="/group.jpg" onClick={getCoordinates}></img>
+  
+    const [inZone, setInZone] = useState(false);
+    const [position, setPosition] = useState({
+        top: 0,
+        left: 0
+    });
+  
+    // TODO: get center coordinates of each face (e.g 904,439) (x: 25, y: 40)
+    function getCoordinates(event) {
+
+        let outlineY = event.clientY - event.target.getBoundingClientRect().top;
+        let outlineX = event.clientX - event.target.getBoundingClientRect().left;
+
+        setPosition({
+            top: outlineY - 40,
+            left: outlineX - 25
+        })
+        
+    }
+
+    // TODO: if hover within radius of center, show outline
+    function checkCoordinates(event) {
+        if (event.clientX < 905 + 25 && 
+            event.clientX > 905 - 25 && 
+            event.clientY < 440 + 40 && 
+            event.clientY > 440 - 40) {
+            setInZone(true);
+        } else {
+            setInZone(false);
+        }
+    }
+
+    return (
+        <div className="container flex flex-col flex-ai-c" >
+            <h1>Guess Who?</h1>
+            <div className="border photo flex flex-col flex-jc-c">
+                <img alt="group" src="/group.jpg" onClick={getCoordinates}></img>
+                <div className="border outline" style={position}></div>
+            </div>
         </div>
-    </div>
-  );
+    );
+
 }
 
-function getCoordinates(event) {
-    console.log(`${event.clientX},${event.clientY}`);
-}
-
-// TODO: get center coordinates of each face
-
-// TODO: if click within radius of center, highlight clicked area
-
-// TODO: when clicked, show pop-up list of name + random names
+// TODO: when clicked, show tooltip of name + random names
 
 // TODO: if selected name correct, return Win message
 
