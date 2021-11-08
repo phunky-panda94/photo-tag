78db36de3a9adce38dce274d893a5ee0c80c0f30
phunky-panda94 <victor.nim94@outlook.com>
2021-11-08 15:24:10 +1100

    implemented outline on click within face zone

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         top: 0,
         left: 0
     });
+
+    const coordinates = [
+        [615, 260],
+        [595, 347],
+        [519, 363]
+    ]
   
     // TODO: get center coordinates of each face (e.g 904,439) (x: 25, y: 40)
     function getCoordinates(event) {
@@ -15,31 +21,41 @@ function App() {
         let outlineY = event.clientY - event.target.getBoundingClientRect().top;
         let outlineX = event.clientX - event.target.getBoundingClientRect().left;
 
-        setPosition({
-            top: outlineY - 40,
-            left: outlineX - 25
-        })
-        
+        console.log(outlineX, outlineY);
+
+        checkCoordinates(outlineX, outlineY);
+
     }
 
     // TODO: if hover within radius of center, show outline
-    function checkCoordinates(event) {
-        if (event.clientX < 905 + 25 && 
-            event.clientX > 905 - 25 && 
-            event.clientY < 440 + 40 && 
-            event.clientY > 440 - 40) {
-            setInZone(true);
-        } else {
-            setInZone(false);
+    function checkCoordinates(x, y) {
+    
+        // check if (x,y) within range of stored coordinates
+
+        for (let coordinate of coordinates) {
+
+            if (x > coordinate[0] - 25 && x < coordinate[0] + 25 &&
+                y > coordinate[1] - 40 && y < coordinate[1] + 40) {
+
+                console.log('in the zone');
+                
+                setInZone(true);
+                setPosition({
+                    left: coordinate[0] - 25,
+                    top: coordinate[1] - 40
+                });
+
+            }
+
         }
     }
 
     return (
         <div className="container flex flex-col flex-ai-c" >
             <h1>Guess Who?</h1>
-            <div className="border photo flex flex-col flex-jc-c">
+            <div className="photo flex flex-col flex-jc-c">
                 <img alt="group" src="/group.jpg" onClick={getCoordinates}></img>
-                <div className="border outline" style={position}></div>
+                {inZone && <div className="outline" style={position}></div>}
             </div>
         </div>
     );
